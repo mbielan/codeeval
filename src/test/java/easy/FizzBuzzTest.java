cd7da1c48b2cdfafc7231dc240ba9f66360eb91b
mbielan <mbielan@o2.pl>
2015-08-29 15:15:39 +0200

    Test for FizzBuzz

---
+++ src/test/java/easy/FizzBuzzTest.java
@@ -0,0 +1,19 @@
+package easy;
+
+import common.AbstractCodeEvalTest;
+import common.CodeEvalTest;
+
+@CodeEvalTest(
+        testClass = FizzBuzz.class,
+        input = {
+                "3 5 10",
+                "2 7 15"
+        },
+        expectedOutput = {
+                "1 2 F 4 B F 7 8 F B",
+                "1 F 3 F 5 F B F 9 F 11 F 13 FB 15"
+        }
+)
+public class FizzBuzzTest extends AbstractCodeEvalTest {
+
+}
